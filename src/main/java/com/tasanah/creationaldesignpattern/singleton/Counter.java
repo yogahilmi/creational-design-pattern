55f8ab1e3cc29d59757d780b812178a179672d80
Yoga Hilmi Tasanah <yoga.assakur@bfi.co.id>
2023-05-23 16:05:48 +0700

    singleton pattern

---
+++ src/main/java/com/tasanah/creationaldesignpattern/singleton/Counter.java
@@ -0,0 +1,14 @@
+package com.tasanah.creationaldesignpattern.singleton;
+
+public class Counter {
+
+    private Long value = 0L;
+
+    public Long getValue() {
+        return value;
+    }
+
+    public synchronized void increment() {
+        value++;
+    }
+}
---
+++ src/test/java/com/tasanah/creationaldesignpattern/singleton/SingletonApplicationTest.java
@@ -0,0 +1,52 @@
+package com.tasanah.creationaldesignpattern.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = SingletonApplication.class)
+class SingletonApplicationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void testSingleton() {
+        var counter1 = applicationContext.getBean(Counter.class);
+        var counter2 = applicationContext.getBean(Counter.class);
+        var counter3 = applicationContext.getBean(Counter.class);
+
+        counter1.increment();
+        counter2.increment();
+        counter3.increment();
+
+        System.out.println(counter1.getValue());
+        System.out.println(counter2.getValue());
+        System.out.println(counter3.getValue());
+    }
+
+    private void incrementCounterAsync(Counter counter, int total) {
+        new Thread(() -> {
+            for (int i = 0; i < total; i++) {
+                counter.increment();
+            }
+        }).start();
+    }
+
+    @Test
+    void testIncrementRaceCondition() throws InterruptedException {
+        var counter1 = applicationContext.getBean(Counter.class);
+        var counter2 = applicationContext.getBean(Counter.class);
+
+        incrementCounterAsync(counter1, 1000000);
+        incrementCounterAsync(counter2, 1000000);
+
+        Thread.sleep(5000L);
+
+        System.out.println(counter1.getValue());
+        System.out.println(counter2.getValue());
+    }
+}
