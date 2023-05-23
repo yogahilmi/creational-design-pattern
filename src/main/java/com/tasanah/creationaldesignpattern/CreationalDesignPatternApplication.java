66250114e7ed7a45101bc1240821610beaf717b0
Yoga Hilmi Tasanah <yoga.assakur@bfi.co.id>
2023-05-23 16:05:14 +0700

    initial commit

---
+++ src/main/java/com/tasanah/creationaldesignpattern/CreationalDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.tasanah.creationaldesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationalDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasanah/creationaldesignpattern/CreationalDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasanah.creationaldesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
